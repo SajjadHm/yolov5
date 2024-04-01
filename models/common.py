502d2ecbb74ba71f0fca1ee7ba307cd842f72983
Sajjad Hashembeiki <sajjad.hashembeigi80@gmail.com>
2024-04-01 17:19:04 +0330

    DW Bottleneck
    Signed-off-by: Sajjad Hashembeiki <sajjad.hashembeigi80@gmail.com>

--- models/common.py
+++ models/common.py
@@ -182,7 +182,7 @@ class Bottleneck(nn.Module):
         super().__init__()
         c_ = int(c2 * e)  # hidden channels
         self.cv1 = Conv(c1, c_, 1, 1)
-        self.cv2 = Conv(c_, c2, 3, 1, g=g)
+        self.cv2 = DWConv(c_, c2, 3, 1)
         self.add = shortcut and c1 == c2
 
     def forward(self, x):
